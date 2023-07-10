ca00eb4503f1361c12b4a016ce3c75e2cbeb256c
“Rajeshwari <kotarajeswari003@gmail.com>
2023-07-10 16:25:04 +0530

    [Rajeshwari kota] Adding Unique elements

--- BasicProgramsOn Array/Program.cs
+++ BasicProgramsOn Array/Program.cs
@@ -11,17 +11,32 @@ namespace BasicProgramsOn_Array
         static void Main(string[] args)
         {
             Console.WriteLine("....PROGRAMS ON ARRAY....");
-            Console.WriteLine("How many elents to add into array");
-            int n = Convert.ToInt32(Console.ReadLine());
-            int[] array = new int[n];
-            Console.WriteLine("storing elements into array");
 
-            for (int i = 0; i < n; i++)
+            Console.WriteLine("\n:DuplicateElements\n2:UniqueElements\n22:Default");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
             {
-                array[i] = Convert.ToInt32(Console.ReadLine());
+                case 1:
+                    Console.WriteLine("How many elents to add into array");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    int[] array = new int[n];
+                    Console.WriteLine("storing elements into array");
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        array[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    CountOfDuplicateElements.Duplicate(array);
+                    break;
+                case 2:
+                    UniqueElements.Elements();
+                    break;
+                default:
+                    Console.WriteLine("Enter the correct option");
+                    break;
             }
-            CountOfDuplicateElements.Duplicate(array);
             Console.ReadLine();
         }
     }
 }
+
---
+++ BasicProgramsOn Array/UniqueElements.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramsOn_Array
+{
+    class UniqueElements
+    {
+        public static void Elements()
+        {
+            Console.WriteLine("How many elents to add into array");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[num];
+            Console.WriteLine("storing elements into array");
+
+            for (int i = 0; i < num; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("The unique elements in the array is:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count == 1)
+                {
+                    Console.WriteLine(arr[i] + " is a unique element.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
