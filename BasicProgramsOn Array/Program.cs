be51829e87598fab70748873c2d2ea01169e3d9d
“Rajeshwari <kotarajeswari003@gmail.com>
2023-07-10 16:51:07 +0530

    [Rajeshwari kota] Adding word reverse

--- BasicProgramsOn Array/Program.cs
+++ BasicProgramsOn Array/Program.cs
@@ -12,7 +12,7 @@ namespace BasicProgramsOn_Array
         {
             Console.WriteLine("....PROGRAMS ON ARRAY....");
 
-            Console.WriteLine("\n:DuplicateElements\n2:UniqueElements\n3:FrequencyOfEachElement\n22:Default");
+            Console.WriteLine("\n:DuplicateElements\n2:UniqueElements\n3:FrequencyOfEachElement\n4:FindingMaximumand Minimum\n5:Word Reverse\n22:Default");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -33,6 +33,23 @@ namespace BasicProgramsOn_Array
                     break;
                 case 3:
                     FrequencyOfEachElement.Frequency();
+                    break;
+                case 4:
+                    Console.WriteLine("How many elents to add into array");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    int[] arr = new int[num];
+                    Console.WriteLine("storing elements into array");
+
+                    for (int i = 0; i < num; i++)
+                    {
+                        arr[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    FindingMaximumandMinimum.MaxOrMin(arr);
+                    break;
+                case 5:
+                    string input = "Raji kota";
+                    string reversed = WordReverse.ReverseWords(input);
+                    Console.WriteLine(reversed);
                     break;
                 default:
                     Console.WriteLine("Enter the correct option");
---
+++ BasicProgramsOn Array/WordReverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramsOn_Array
+{
+    internal class WordReverse
+    {
+        public static string ReverseWords(string input)
+        {
+            string[] words = input.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] characters = words[i].ToCharArray();
+                Array.Reverse(characters);
+                words[i] = new string(characters);
+            }
+            return string.Join(" ", words);
+        }
+    }
+}
