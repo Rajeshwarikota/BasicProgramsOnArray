fa46f98ffe8237e98a2914052105a27b4fb6157e
“Rajeshwari <kotarajeswari003@gmail.com>
2023-07-10 14:40:01 +0530

    [Rajeshwari kota] Adding Basic programs on Array

---
+++ BasicProgramsOn Array/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramsOn_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("....PROGRAMS ON ARRAY....");
+            Console.ReadLine();
+        }
+    }
+}
